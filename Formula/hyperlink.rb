48caf7422cdbbb882a60a2b216e89a1d4be1f85b
Chaoos-404 <chenchijiun0617@gmail.com>
2026-06-24 19:52:46 +0800

    Update Hyperlink to 0.1.3

--- Formula/hyperlink.rb
+++ Formula/hyperlink.rb
@@ -1,17 +1,17 @@
 class Hyperlink < Formula
   desc "High-speed peer-to-peer file transfer over USB4 and Thunderbolt network links"
   homepage "https://github.com/Chaoos-404/HyperLink"
-  version "0.1.2"
+  version "0.1.3"
   license "MIT"
 
   on_macos do
-    url "https://github.com/Chaoos-404/HyperLink/releases/download/v0.1.2/Hyperlink-0.1.2-macos-universal.tar.gz"
-    sha256 "bf953d3ca9c2f8960a85b114da48b0b9cb0762870c145f0ac650b3fe770e82ef"
+    url "https://github.com/Chaoos-404/HyperLink/releases/download/v0.1.3/Hyperlink-0.1.3-macos-universal.tar.gz"
+    sha256 "6487092254678017d62082ff6defa0ba09a7945d4d570b853b5766ebf167caa7"
   end
 
   on_linux do
-    url "https://github.com/Chaoos-404/HyperLink/releases/download/v0.1.2/Hyperlink-0.1.2-linux-x64.tar.gz"
-    sha256 "a0761561759df49071db431b0c54c6271d0546e70fdc8ebf4ea189fdc25b55a4"
+    url "https://github.com/Chaoos-404/HyperLink/releases/download/v0.1.3/Hyperlink-0.1.3-linux-x64.tar.gz"
+    sha256 "8454d486aba210f48f52addf5e7740d3768c08aba8ed5bea3d2f1a7a66cd96a1"
   end
 
   def install
